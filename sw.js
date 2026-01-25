3c2cb80aac73a2ac43125c899d0d0b890997319c
alassio-eng <alassio@gmail.com>
2026-01-25 18:35:07 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+// Service Worker per Zbike 2 PWA
+const CACHE_NAME = 'zbike2-v1';
+const urlsToCache = [
+  './zbike2-PWA.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
